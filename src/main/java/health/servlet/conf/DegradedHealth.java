dae75f8af4397bbb342f6f09a4c377541ca9e978
Rik Ende <rivanden@cisco.com>
2019-01-24 16:14:51 -0500

    test

--- src/main/java/health/servlet/conf/DegradedHealth.java
+++ src/main/java/health/servlet/conf/DegradedHealth.java
@@ -2,10 +2,12 @@ package health.servlet.conf;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 import static health.servlet.conf.Tool.sleep;
 
+@Component
 public class DegradedHealth implements ReactiveHealthIndicator {
 
   @Override
