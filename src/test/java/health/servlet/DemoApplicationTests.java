5882c3ec98495043cd58cdc0f748832f710a7338
Rik Ende <rivanden@cisco.com>
2019-01-24 15:22:51 -0500

    test

--- src/test/java/health/servlet/DemoApplicationTests.java
+++ src/test/java/health/servlet/DemoApplicationTests.java
@@ -2,16 +2,32 @@ package health.servlet;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebFluxTest
 public class DemoApplicationTests {
+	@Autowired
+	private WebTestClient webClient;
 
 	@Test
-	public void contextLoads() {
+	public void get() throws Exception {
+		webClient.get().uri("/")
+				.exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBody().equals("Hello");
 	}
+
+	@Test
+	public void health() throws Exception {
+		webClient.get().uri("/actuator/health")
+				.exchange()
+				.expectStatus().is5xxServerError();
+	}
+
 
 }
 
