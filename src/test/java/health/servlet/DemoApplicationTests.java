6a56ec57f122f7a5d6220c1992010129b120e5be
Rik Ende <rivanden@cisco.com>
2019-01-24 16:18:43 -0500

    test

--- src/test/java/health/servlet/DemoApplicationTests.java
+++ src/test/java/health/servlet/DemoApplicationTests.java
@@ -3,7 +3,6 @@ package health.servlet;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.context.junit4.SpringRunner;
