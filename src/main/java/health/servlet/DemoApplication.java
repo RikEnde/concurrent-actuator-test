d6c3528406c6a9cd8bea9080ea23fa167bb3eb46
Rik Ende <rivanden@cisco.com>
2019-02-06 16:01:19 -0500

    test customizability

--- src/main/java/health/servlet/DemoApplication.java
+++ src/main/java/health/servlet/DemoApplication.java
@@ -4,22 +4,15 @@ package health.servlet;
 
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.actuate.health.ConcurrentCompositeHealthIndicator;
-import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthAggregator;
 import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.actuate.health.HealthIndicatorRegistry;
-import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.time.Duration;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.springframework.boot.actuate.health.Status.DOWN;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -28,16 +21,6 @@ public class DemoApplication {
         new SpringApplicationBuilder(DemoApplication.class).web(WebApplicationType.SERVLET).run(args);
     }
 
-
-    @Bean
-    public HealthAggregator healthAggregator() {
-        return (healths) -> {
-                List<Status> statusCandidates = healths.values().stream().map(Health::getStatus)
-                    .collect(Collectors.toList());
-                Status status = statusCandidates.contains(Status.UP) ? new Status("NOT ALL BAD") : DOWN;
-                return new Health.Builder(status, new LinkedHashMap<>(healths)).build();
-        };
-    }
 
     @Bean
     public ThreadPoolTaskExecutor executor() {
